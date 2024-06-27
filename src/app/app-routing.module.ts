7a82102a011323a681f66881ae8b5756c8a3ecf8
AJAYJANGID07 <ajayjangid310@gmail.com>
2024-06-27 12:35:18 +0530

    routing set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CharaterComponentComponent } from './components/character-component/character-component.component';
+import { CharacterListComponent } from './components/character-component/character-list/character-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: CharacterListComponent, 
+    children: [
+      {
+        path: 'character-list', component: CharacterListComponent
+      },
+    ]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharaterComponentComponent } from './components/character-component/character-component.component';
+import { CharacterListComponent } from './components/character-component/character-list/character-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharaterComponentComponent,
+    CharacterListComponent
   ],
   imports: [
     BrowserModule,
